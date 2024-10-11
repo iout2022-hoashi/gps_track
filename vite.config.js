2892087fe24e38466d4e371cb06d2a18859390fc
iout2022-hoashi <tk220041dai.hoashi@gmail.com>
2024-10-11 13:29:00 +0900

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(() => ({
   server: {
     port: 3000,
+    open: false,
     proxy: {
       '/api/socket': 'ws://localhost:8082',
       '/api': 'http://localhost:8082',
